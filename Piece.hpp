ae63a40ce7c75b04650daf6aad28055afb2e179a
Malik Tremain <mt1704@york.ac.uk>
2023-01-11 15:35:51 +0000

    complete restart

--- Piece.hpp
+++ Piece.hpp
@@ -1,15 +1,19 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <cctype>
+namespace piece_types
+{
+	 constexpr uint8_t White = 0b10000000;
+	 constexpr uint8_t Black = 0b01000000;
+	 constexpr uint8_t King = 0b00100000;
+	 constexpr uint8_t Queen = 0b00010000;
+	 constexpr uint8_t Bishop = 0b00001000;
+	 constexpr uint8_t Knight = 0b00000100;
+	 constexpr uint8_t Rook = 0b00000010;
+	 constexpr uint8_t Pawn = 0b00000001;
+}
 class Piece
 {
 public:
-	static constexpr uint8_t White  = 0b10000000;
-	static constexpr uint8_t Black  = 0b01000000;
-	static constexpr uint8_t King   = 0b00100000;
-	static constexpr uint8_t Queen  = 0b00010000;
-	static constexpr uint8_t Bishop = 0b00001000;
-	static constexpr uint8_t Knight = 0b00000100;
-	static constexpr uint8_t Rook   = 0b00000010;
-	static constexpr uint8_t Pawn   = 0b00000001;
+	
 };
